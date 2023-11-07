2ddbbd067d893325fc6a540246291055658bfdb4
thiagolms <thiagopinheiiro@live.com>
2023-11-07 11:56:51 -0300

    botão login / botão infoUser não aparece se não logar

--- js/logout.js
+++ js/logout.js
@@ -1,10 +1,15 @@
 const userToken = localStorage.getItem('token');
 const loginButton = document.getElementById('login-menu-item');
+const infoButton = document.getElementById('minhaConta-menu-item');
 if (userToken) {
-    loginButton.innerHTML = '<a class="nav-link" href="#" id="logout-link"><i class="fa-solid fa-right-from-bracket" aria-hidden="true"></i> Sair</a>';
+    infoButton.innerHTML = '<li class="nav-item" id="minhaConta-menu-item"><a class="nav-link" href="infoUser.html"><i class="fa fa-fw fa-paw"aria-hidden="true"></i>Minha conta</a></li>';
+    loginButton.innerHTML = '<a class="nav-link" href="#" id="logout-link"><i class="fa-solid fa-right-from-bracket" aria-hidden="true"></i>Sair</a>';
     const logoutLink = document.getElementById('logout-link');
     logoutLink.addEventListener('click', () => {
         localStorage.removeItem('token');
         window.location.href = 'login.html';
     })
+}
+if (!userToken) {
+    infoButton.style.display = 'none';
 } 
